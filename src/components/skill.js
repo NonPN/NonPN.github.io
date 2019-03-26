2096b083e1f006d67d86512ed3ac9565316a7147
Non <non9441@gmail.com>
2019-03-26 16:34:24 +0700

    add more skills :sparkles:

--- src/components/skill.js
+++ src/components/skill.js
@@ -4,6 +4,10 @@ import java from '../images/java.svg'
 import js from '../images/js.svg'
 import cprog from '../images/cprog.svg'
 import python from '../images/python.svg'
+import html from '../images/html.svg'
+import css from '../images/css.svg'
+import node from '../images/node.svg'
+import react from '../images/react.svg'
 import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 
 const Block = styled.div`
@@ -15,12 +19,23 @@ const TitleLine = styled.div`
 `
 const Title = styled.h3`
     font-family: 'Archivo Black', sans-serif;
-    font-size: 35px;
+    font-size: 25px;
     margin: 20px;
 `
 const Container = styled.div`
     display: flex;
     justify-content: center;
+`
+const HeadLine = styled.div`
+    border-top-left-radius: 20px;
+    border-top-right-radius: 20px;
+    background-color: #ff9b21;
+`
+
+const Head = styled.h3`
+    font-family: 'Archivo Black', sans-serif;
+    margin: 20px;
+    padding-top: 30px;
 `
 
 const Logo = styled.div`
@@ -31,12 +46,21 @@ export default class Skill extends Component {
     render() {
         return(
             <Block>
-                <TitleLine><Title>Skills</Title></TitleLine>
+                <HeadLine><Head>Skills</Head></HeadLine>
+                <TitleLine><Title>Programming Language</Title></TitleLine>
                 <Container>
                     <DropdownLogo id='java' logo={java}/>
                     <DropdownLogo id='js' logo={js}/>
                     <DropdownLogo id='cprog' logo={cprog}/>
                     <DropdownLogo id='python' logo={python}/>
+                </Container>
+                <hr/>
+                <TitleLine><Title>Web appilcation</Title></TitleLine>
+                <Container>
+                    <DropdownLogo id='html' logo={html}/>
+                    <DropdownLogo id='css' logo={css}/>
+                    <DropdownLogo id='node' logo={node}/>
+                    <DropdownLogo id='react' logo={react}/>
                 </Container>
             </Block>
         )
